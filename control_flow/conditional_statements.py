57a1cc4b5f360692d25387e95c6b75c8c2188200
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-08 16:29:14 +0300

    The condition statements in python

---
+++ control_flow/conditional_statements.py
@@ -0,0 +1,11 @@
+# The conditional statements in python
+# if, elif, else
+# Example code
+temp = input("temp: ")
+if int(temp) > 35:
+    print("its hot \nDrink water")
+elif int(temp) > 20:
+    print("It a normal temperature")
+else:
+    print("its a cold day")
+print("Done")
