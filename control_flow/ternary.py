efc6df44ea6008f3accf0dc9a39edc69038d03d4
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-09 02:04:12 +0300

    bug fix

--- control_flow/ternary.py
+++ control_flow/ternary.py
@@ -1,4 +1,4 @@
 # How to use ternary conditional operator in python
-age = eval(input("Age: "))
-message = "Eligible" if age >= 18 else "Not Eligible"
+age = (input("Age: "))
+message = "Eligible" if int(age) >= 18 else "Not Eligible"
 print(message)
