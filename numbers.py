8f44844bbeef406b477ecae2eee1edd60bcba258
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-08 14:49:32 +0300

    numbers and their arithmetic operations in python

---
+++ numbers.py
@@ -0,0 +1,12 @@
+# Numbers in python: integers, float and complex numbers
+# Numbers arithmetics
+# Augmented assignment operator
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 ** 2)
+x = 5
+x += 3
+print(x)
