fea610779011a2c00d1673137f469bd08a64d0c6
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-08 15:26:49 +0300

    type conversions in python programming

---
+++ type_conversion.py
@@ -0,0 +1,12 @@
+# check the type of data
+# convert from one data type to another
+# bool type conversion
+x = input("x: ")
+print(type(x))
+y = int(x) + 1
+print(f"x: {x} y: {y}")
+print(type(x))
+print(bool(0))
+print(bool(""))
+print(bool("false"))
+print(bool(None))
