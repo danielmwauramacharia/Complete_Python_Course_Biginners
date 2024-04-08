713f2f02435aaa472b1478e9733949e0121f8a26
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-08 15:35:09 +0300

    moed to a folder "primitive_types"

--- formatted_string.py
+++
@@ -1,7 +0,0 @@
-# using the formatted string in python
-# The f string is calculated at run time and
-# Any valid expression can be placed in the braces
-first_name = "Daniel"
-last_name = "Mwaura"
-full_name = f"{first_name} {last_name}"
-print(full_name)
--- working_with_numbers.py
+++
@@ -1,8 +0,0 @@
-# Working with numbers in python
-# use of builtin functions
-# use of math module
-import math
-print(round(2.9))
-print(abs(-3.4))
-print(math.ceil(2.3))
-print(math.comb(5, 3))
