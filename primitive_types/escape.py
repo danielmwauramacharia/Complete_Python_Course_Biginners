66fd04cbe1d1278550d0a5a4bbdff1a458fde913
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-08 15:32:59 +0300

    Lessons on module 1

---
+++ primitive_types/escape.py
@@ -0,0 +1,9 @@
+# The Escape Sequence in Python
+# \" print the double quote
+# \' print the single quote
+# \\ print one backslash
+# \n a new line
+print("A single \' quote")
+print("A double \" quote")
+print("print one \\ backslash")
+print("line one \nThis is a new line")
