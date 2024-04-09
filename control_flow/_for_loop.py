26914aa226db12337c0966547040ed534f64a98d
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-09 03:53:19 +0300

    comments added

---
+++ control_flow/_for_loop.py
@@ -0,0 +1,9 @@
+# for loops creates repetition for a piece of code
+for number in range(3):
+    print("Attempt", number)
+# with a starting point
+for number in range(1, 3):
+    print("Attempt", number)
+# with a step in the iteration
+for number in range(1, 10, 2):
+    print("Attempt", number)
