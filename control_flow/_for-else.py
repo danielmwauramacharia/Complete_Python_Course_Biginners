01c0146579e0d2124d9a62251a50f622537482a6
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-09 04:07:39 +0300

    for else loop in python

---
+++ control_flow/_for-else.py
@@ -0,0 +1,9 @@
+# for else loop in python
+success = False
+for number in range(3):
+    print("Attempt")
+    if success:
+        print("success")
+        break
+else:
+    print("Tried 3 times unsuccesfully")
