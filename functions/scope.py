8953339716e2fa495e466c1b31542ba96373e772
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-09 13:18:04 +0300

    local and global variables in python

---
+++ functions/scope.py
@@ -0,0 +1,12 @@
+# There are local and global variables
+# Local variables are local to a function
+# Global can be accessed anywhere in the program
+# Global variables should be used with care
+message = "a"  # global
+
+
+def greet(name):
+    massage = "hi"  # local
+
+
+print(message)
