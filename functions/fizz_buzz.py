68d27871173465aa2764b3f2e884409f260ce682
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-09 17:02:03 +0300

    update the variable names

--- functions/fizz_buzz.py
+++ functions/fizz_buzz.py
@@ -4,16 +4,16 @@
 # prints fizz buzz if divisible by 3 and 5
 # Return input if not divisible by 3 and 5
 
-def fizz_buzz(input):
-    if input % 3 == 0 and input % 5 == 0:
+def fizz_buzz(num):
+    if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
-    elif input % 3 == 0 and input % 5 != 0:
+    elif num % 3 == 0 and num % 5 != 0:
         print("Fizz")
-    elif input % 5 == 0 and input % 3 != 0:
+    elif num % 5 == 0 and num % 3 != 0:
         print("Buzz")
     else:
-        print(input)
+        print(num)
 
 
-num = input("Enter positive integer: ")
-fizz_buzz(int(num))
+number = input("Enter positive integer: ")
+fizz_buzz(int(number))
