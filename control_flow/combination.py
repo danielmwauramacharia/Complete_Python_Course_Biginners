f0319185586ce2a30ace41922549e27f520b44c9
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-14 14:50:00 +0300

    two digit combination

---
+++ control_flow/combination.py
@@ -0,0 +1,10 @@
+x = 0
+for i in range(0, 10):
+    for n in range(x, 10):
+        if i == n:
+            continue
+        if i < 8:
+            print("{}{}, ".format(i, n), end='')
+        else:
+            print("{}{}{}".format(i, n, "\n"), end='')
+    x += 1
