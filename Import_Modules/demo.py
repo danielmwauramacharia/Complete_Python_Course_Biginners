b899aa5d66713df8f6879dec9756b88a27de722c
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-19 09:34:05 +0300

    commandline arguments

---
+++ Import_Modules/demo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+length = len(sys.argv)
+if __name__ == "__main__":
+    if length <= 1:
+        print("{} argument.".format(length-1))
+    if length == 2:
+        print("{} argument:".format(length-1))
+    else:
+        print("{} arguments:".format(length-1))
+    for i in range(1, length):
+        print("{}{} {}".format(i, ":", sys.argv[i]))
